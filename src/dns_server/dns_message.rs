4df9ca9beacb5985e4bc3f454fef7a2c39de3414
YJS <y.j3ms.n@gmail.com>
2023-12-30 03:51:40 +0900

    fix: DnsMessage::from now considers enum variants

--- src/dns_server/dns_message.rs
+++ src/dns_server/dns_message.rs
@@ -207,10 +207,17 @@ impl From<Vec<u8>> for DnsMessage {
             .expect("Error while parsing request question");
         let dns_answers = Self::parse_answer(&byte_vec, dns_header.qdcount, consumed_amount);
 
-        Self::DnsRequest(DnsMessageForm {
-            dns_header,
-            dns_questions,
-            dns_answers,
-        })
+        match dns_answers {
+            Some(_) => Self::DnsResponse(DnsMessageForm {
+                dns_header,
+                dns_questions,
+                dns_answers,
+            }),
+            None => Self::DnsRequest(DnsMessageForm {
+                dns_header,
+                dns_questions,
+                dns_answers,
+            }),
+        }
     }
 }
